7f3e7e1c9d000be8c7fdf0148f8c1e9fa4a24ed2
Pisarski Roman <pisarski.roman@gmail.com>
2024-01-04 19:33:30 +0100

    list

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,8 @@
 const list = document.querySelectorAll('h2')
+const listUl = document.querySelectorAll('ul')
 console.log(`Number of categories: ${list.length}`)
+console.log(listUl)
+list.forEach(function callback(element) {
+    console.log(element)
+});
+
