320652a783a270b7396ed91832578a6da5ea2b7d
Pisarski Roman <pisarski.roman@gmail.com>
2024-01-05 19:14:32 +0100

    taks 1 finish

--- js/task-01.js
+++ js/task-01.js
@@ -1,7 +1,16 @@
-const list = document.querySelectorAll('h2')
-const nameList=[]
-console.log(`Number of categories: ${list.length}`)
-list.forEach(function callback(element) {
-    nameList.push(element.textContent)
+
+// pobieram wszytkie elementy dla sekcji zwierającej klasę item
+const section = document.querySelectorAll('.item')
+// wyswietlam długość tej listy
+console.log(`Number of categories: ${section.length}`)
+
+// dla kazdego elementy z listy section 
+section.forEach(element => {
+    // wybieram tylko nagłówek h2 i wysietlam tylko zawarty w nim tekst
+    const categories = element.querySelector('h2')
+    console.log(`Category: ${categories.textContent}`)
+    // wybieram wszysttkie elementy li zawarte w ul i wysietlam dlugosć listy
+    const list = element.querySelectorAll('ul li')
+    console.log(`Elements: ${list.length}`)
+    
 });
-console.log(nameList)
