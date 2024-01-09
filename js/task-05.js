0066b1cea5ff3a1cf9c9c6d7d59e541689f41151
Pisarski Roman <pisarski.roman@gmail.com>
2024-01-09 18:34:41 +0100

    tekt area

--- js/task-05.js
+++ js/task-05.js
@@ -1,18 +1,20 @@
+// wyszukuję input tekst
 const textInput = document.querySelector('[id="name-input"]')
+// wyszukuję output tekst
 const textOutput= document.querySelector('[id="name-output"]')
 
+//funkcja sprawdzająca zawartość pola tekstowego i zwracająca wynik
 textInput.addEventListener("input", (event) => {
+    //wartość pola tekstowego
     const value = event.currentTarget.value
     
+    //warunek sprawdzenia i wyswietlenia wyniku
     if (value.length === 0) {
         textOutput.textContent = "Anonymous";
-        console.log('same 0');
-        console.log(value.length);
     }
     
     else {
         textOutput.textContent = value
-        console.log(value)
     }
 
 });
