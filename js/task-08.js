f6d5ad49db09697dc29d97a787582d14e158f72b
Pisarski Roman <pisarski.roman@gmail.com>
2024-01-10 19:08:32 +0100

    aa

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
+const formInput = document.querySelector('.login-form')
+
+formInput.addEventListener("submit", formSubmit);
+
+let dataUser = {};
+
+function formSubmit(event) {
+    event.preventDefault();
+
+    const userEmail = formInput.elements.email.value;
+    const userPassword = formInput.elements.password.value;
+    
+    if (userEmail === "" || userPassword === "") {
+        alert("Please fill in all the fields!");
+    }
+    else {
+        dataUser['email'] = userEmail;
+        dataUser.password = userPassword;
+        console.log(dataUser);
+    }
+    formInput.reset();
+}
+
 
