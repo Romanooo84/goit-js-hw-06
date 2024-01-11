0967141ece4f0ae415c0a9ed50775f55f595b499
Pisarski Roman <pisarski.roman@gmail.com>
2024-01-11 18:46:50 +0100

    zad 9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const button = document.querySelector('button')
+const bgColor = document.querySelector(".color")
+const body = document.querySelector("body")
+
+button.addEventListener("click", (event) => {
+  const color = getRandomHexColor();
+  bgColor.textContent = color;
+  body.style.backgroundColor = color
+});
+  
+
+  
