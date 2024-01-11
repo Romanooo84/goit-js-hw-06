d8810db204f4f26e2156db93efc5733a6721a83e
Pisarski Roman <pisarski.roman@gmail.com>
2024-01-11 20:30:46 +0100

    zadanie 10

--- js/task-10.js
+++ js/task-10.js
@@ -16,13 +16,11 @@ let boxLength = 0
 
 buttonCreate.addEventListener("click", () => {
   value = textInput.value;
-  console.log(value);
   createBoxes(value)
 })
 
 buttonDestroy.addEventListener("click", () => {
-  console.log('guzik działa')
-  destroyBoxes(boxLength);
+  destroyBoxes();
 });
 
 
@@ -30,21 +28,17 @@ function createBoxes(value) {
   for (var i = 1; i <= value; i++){
     let divHeight = 20 + i * 10;
     let divWidth = divHeight  ;
-    let divPart = `<div style =" width:${divWidth}px; height:${divHeight}px; background-color:${getRandomHexColor()}">div</div>`;
+    let divPart = `<div style =" width:${divWidth}px; height:${divHeight}px; background-color:${getRandomHexColor()}"></div>`;
     divList.push(divPart);
   }
   const newList = divList.join('')
   divBoxes.insertAdjacentHTML("beforeend", newList);
-  boxLength = divBoxes.length
-  console.log(boxLength)
+  boxLength = divBoxes.children.length
+  divList = []
 }
 
-function destroyBoxes(boxLength) {
-  console.log('funkcja uruchamia się')
-  console.log(boxLength)
-  for (var i = 1; i = boxLength; i++){
-    console.log(i)
-    console.log(divBoxes.lastChild)
+function destroyBoxes() {
+  while (divBoxes.lastChild){
     divBoxes.removeChild(divBoxes.lastChild);
   }
 }
